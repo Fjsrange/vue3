44690d207278f617397555cb5ebf09e7651c3377
Orange <106434581+Fjsrange@users.noreply.github.com>
2024-07-04 00:05:17 +0800

    尚硅谷-张天禹，vue3，p12-p15

---
+++ 备份/10_ref对比reactive/components/person.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="person">
+    <h2>汽车信息：一辆{{ car.brand }}车的价值{{ car.price }}</h2>
+    <button @click="changeBrand">修改汽车的品牌</button>
+    <button @click="changePrice">修改汽车的价格</button>
+    <button @click="changeCar">修改汽车</button>
+    <hr />
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="person">
+import { ref, reactive } from "vue";
+// 数据
+let car = ref({ brand: "奔驰", price: 100 });
+let sum = ref(0);
+
+function changeBrand() {
+  car.value.brand = "宝马";
+}
+function changePrice() {
+  car.value.price += 10;
+}
+function changeCar() {
+  // car = { brand: "宝马", price: 200 }; // 无法实现修改对象的功能
+  // car = reactive({ brand: "宝马", price: 200 }); // 无法实现修改对象的功能
+
+  // assign的功能是将后面所有的属性(key: value)都加到第一个里面
+  // Object.assign(car, obj1, obj2, obj3);
+  // Object.assign(car, { brand: "宝马", price: 200 });
+
+  car.value = { brand: "宝马", price: 200 };
+}
+function changeSum() {
+  sum.value += 1;
+}
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 10px;
+}
+</style>
---
+++ 备份/9_ref创建_对象类型的响应式数据/components/person.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="person">
+    <h2>汽车信息：一辆{{ car.brand }}车的价值{{ car.price }}</h2>
+    <button @click="changePrice">修改汽车的价格</button>
+    <br />
+    <h2>游戏列表：</h2>
+    <ul>
+      <li v-for="item in games" :key="item.id">{{ item.name }}</li>
+    </ul>
+    <button @click="changeGameName">修改第一个游戏的名字</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="person">
+import { ref } from "vue";
+// 数据
+let car = ref({ brand: "奔驰", price: 100 });
+let games = ref([
+  { id: "001", name: "英雄联盟" },
+  { id: "002", name: "穿越火线" },
+  { id: "003", name: "地下城与勇士" },
+  { id: "004", name: "魔兽世界" },
+]);
+
+function changePrice() {
+  car.value.price += 10;
+}
+
+function changeGameName() {
+  games.value[0].name = "Apex";
+}
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 10px;
+}
+</style>
