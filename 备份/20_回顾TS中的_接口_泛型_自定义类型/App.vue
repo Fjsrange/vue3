500721e57a78c453a459b111aea6b1be78a1bf4c
Orange <106434581+Fjsrange@users.noreply.github.com>
2024-07-06 00:33:01 +0800

    尚硅谷-张天禹，vue3，p25-p26

---
+++ 备份/20_回顾TS中的_接口_泛型_自定义类型/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Person ref="ren" />
+</template>
+
+<script lang="ts" setup name="App">
+import Person from "./components/person.vue";
+</script>
+
+<style scoped></style>
---
+++ 备份/20_回顾TS中的_接口_泛型_自定义类型/components/person.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="person">???</div>
+</template>
+
+<script lang="ts" setup name="person">
+import { type PersonInter, type Persons } from "@/types";
+// let person: PersonInter = { id: "001", name: "张三", age: 18 };
+
+// 定义一个数组，数组中的元素是PersonInter类型
+let personList: Persons = [
+  { id: "001", name: "张三", age: 18 },
+  { id: "002", name: "李四", age: 19 },
+  { id: "003", name: "王五", age: 20 },
+];
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin: 10px;
+}
+</style>
